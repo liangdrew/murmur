354e12e4f098845897c6a50d8f74f7ff969ea6a7
liangdrew <liang.andrew@hotmail.com>
2016-08-30 17:10:32 -0400

    Reimplement server code using low-level server-side API

--- src/main/scala/com/liangdrew/murmur/Server.scala
+++ src/main/scala/com/liangdrew/murmur/Server.scala
@@ -2,13 +2,14 @@ package com.liangdrew.murmur
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.Http.ServerBinding
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.HttpMethods._
 import akka.stream.ActorMaterializer
-import com.liangdrew.murmur.services.TestService
+import akka.stream.scaladsl.Sink
 import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.concurrent.Future
 import scala.io.StdIn
 
 object Server extends App {
@@ -16,7 +17,7 @@ object Server extends App {
     // Actor needs ActorSystem to operate
     implicit val system = ActorSystem("murmur")
     // Flow needs ActorMaterializer to stream data
-    implicit val materializer = ActorMaterializer
+    implicit val materializer = ActorMaterializer()
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
@@ -24,10 +25,35 @@ object Server extends App {
     val hostname = systemConfigs.getString("murmur.http.hostname")
     val port = systemConfigs.getInt("murmur.http.port")
 
-    val route = TestService.route
+    val serverSource = Http().bind(interface = hostname, port = port)
 
-    val bindingFuture = Http().bindAndHandle(route, hostname, port)
-    println(s"Server is up at http://$hostname:$port\nPress RETURN to stop...")
+    val requestHandler: HttpRequest => HttpResponse = {
+        case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+            HttpResponse(entity = HttpEntity(
+                ContentTypes.`text/html(UTF-8)`, "<html><body>Hello world!</body></html>")
+            )
+        case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+            HttpResponse(entity = "PONG!")
+        case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+            HttpResponse(entity = "BOOM!")
+        case r: HttpRequest =>
+            // Drain incoming HTTP entity stream
+            r.discardEntityBytes()
+            HttpResponse(404, entity = "Unknown resource!")
+    }
+
+    val bindingFuture: Future[ServerBinding] =
+        serverSource.to(Sink.foreach { connection =>
+            println("Accepted new connection from " + connection.remoteAddress)
+
+            connection handleWithSyncHandler requestHandler
+        }).run()
+
+    bindingFuture.onFailure {
+        case ex: Exception =>
+            println(s"Failed to bind to $hostname:$port")
+    }
+    println(s"Server is up at http://$hostname:$port\nPress RETURN to shut down server...")
     StdIn.readLine() // let it run until user presses return
 
     bindingFuture
